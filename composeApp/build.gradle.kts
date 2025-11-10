da876419f794f5f673c324989c10ae9019f4a4ad
Muhammet Gündoğar <muhammetemingundogar53@gmail.com>
2025-11-10 10:17:13 +0300

    feat: Bump app version to 1.2
    This commit updates the application version for Android.
    
    - The `versionCode` is incremented from 2 to 3.
    - The `versionName` is changed from "1.1" to "1.2".

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -78,8 +78,8 @@ android {
         applicationId = "com.gundogar.shoplist"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
     packaging {
         resources {
