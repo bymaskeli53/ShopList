021868293c0e91e6f60251e1a2e53dded8c3ba51
Muhammet Gündoğar <muhammetemingundogar53@gmail.com>
2026-08-03 23:38:14 +0300

    feat: Bump app version and SDK targets
    This commit updates the application version and upgrades the Android SDK targets.
    
    - **`build.gradle.kts`**:
        - The `versionCode` is incremented from 4 to 5.
        - The `versionName` is changed from "1.3" to "1.4".
    - **`libs.versions.toml`**:
        - Updated `android-compileSdk` and `android-targetSdk` from 35 to 36.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -78,8 +78,8 @@ android {
         applicationId = "com.gundogar.shoplist"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
     }
     packaging {
         resources {
